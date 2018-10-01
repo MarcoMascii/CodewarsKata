03ff6543a48322a25af4f4584d67e69b0f1c7b9b
Marco <Marco@DESKTOP-MLGG85L>
2018-10-01 15:53:05 +0200

    Added Alphabetic Anagram solution

---
+++ CodewarsSolution/CodewarsProject/Katas/AlphabeticAnagrams.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodewarsProject
+{
+    public class AlphabeticAnagrams
+    {
+        public static long ListPosition(string value)
+        {
+
+            List<char> sortedLetters = value.ToCharArray().ToList();
+            sortedLetters.Sort();
+            Dictionary<char, int> frequencies = new Dictionary<char, int>();
+            long index = 1;
+            int remainingLetters = value.Length - 1;
+            int splitStringIndex = 0;
+            foreach (char letter in sortedLetters.Distinct())
+            {
+                frequencies.Add(letter, sortedLetters.Count(x => x == letter));
+            }
+
+            while (sortedLetters.Count > 0)
+            {
+                for (var i = 0; i < sortedLetters.Count; i++)
+                {
+                    if (sortedLetters[i] != value[splitStringIndex])
+                    {
+                        if (sortedLetters[i] != sortedLetters[i + 1])
+                        {
+                            var permutations = Factorial(remainingLetters);
+                            index += permutations / GetSubPermutations(frequencies, sortedLetters[i]);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        splitStringIndex++;
+                        frequencies[sortedLetters[i]]--;
+                        sortedLetters.RemoveRange(i, 1);
+                        remainingLetters--;
+                        break;
+                    }
+                }
+            }
+
+            return index;
+        }
+
+        private static long GetSubPermutations(Dictionary<char, int> frequencies, char currentLetter)
+        {
+            frequencies[currentLetter]--;
+            long denominator = 1;
+            foreach (var key in frequencies)
+            {
+                var subPermutations = Factorial(frequencies[key.Key]);
+                 denominator *= subPermutations != 0 ? subPermutations : 1;
+            }
+            frequencies[currentLetter]++;
+            return denominator;
+        }
+
+        private static long Factorial(int n)
+        {
+            int temp = n;
+            long output = n;
+            while (temp-- > 1)
+            {
+                output *= temp;
+            }
+            return output;
+        }
+    }
+}
---
+++ CodewarsSolution/CodewarsTests/Tests/AlphabeticAnagramsTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodewarsProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsTests
+{
+    [TestClass]
+    public class AlphabeticAnagramsTest
+    {
+        [TestMethod]
+        public void test1()
+        {
+            TestNumberToOrdinal("A", 1);
+        }
+        [TestMethod]
+        public void test2()
+        {
+            TestNumberToOrdinal("ABAB", 2);
+        }
+        [TestMethod]
+        public void test3()
+        {
+            TestNumberToOrdinal("AAAB", 1);
+        }
+        [TestMethod]
+        public void test4()
+        {
+            TestNumberToOrdinal("BAAA", 4);
+        }
+        [TestMethod]
+        public void test5()
+        {
+            TestNumberToOrdinal("QUESTION", 24572);
+        }
+        [TestMethod]
+        public void test6()
+        {
+            TestNumberToOrdinal("BOOKKEEPER", 10743);
+        }
+
+        public void TestNumberToOrdinal(string value, long expected)
+        {
+            Assert.AreEqual(expected, AlphabeticAnagrams.ListPosition(value), string.Format("Input {0}", value));
+        }
+    }
+}
