05ceb827929db30161365191ed8ef420390d0fd8
Marco <Marco@DESKTOP-MLGG85L>
2018-10-23 10:37:05 +0200

    updates

--- CodewarsSolution/CodewarsProject/Katas/TotalIncreasingOrDecreasingNumbers.cs
+++ CodewarsSolution/CodewarsProject/Katas/TotalIncreasingOrDecreasingNumbers.cs
@@ -8,7 +8,7 @@ namespace CodewarsProject
         {
             BigInteger result = 1;
             BigInteger temp = BigInteger.One;
-            
+
             for (int i = 1; i <= 10; i++)
             {
                 temp *= (x + i);
@@ -26,53 +26,6 @@ namespace CodewarsProject
             temp /= 362880;
             result += temp - 2 - 10 * x;
 
-            return result;
-
-            int[,] decrease = new int[x, 10];
-            int[,] increase = new int[x, 10];
-
-            for (int k = 0; k < 9; k++)
-            {
-                decrease[0, k] = 1;
-                increase[0, k] = 1;
-            }
-
-            
-            for (int i = 1; i < x; i++)
-            {
-                for (int current = 0; current <= 9; current++)
-                {
-                    decrease[i, current] = 0;
-                    for (int smaller = 0; smaller < current; smaller++)
-                    {
-                        decrease[i, current] = (decrease[i, current] + decrease[i - 1, smaller]);
-                    }
-                    increase[i, current] = 0;
-                    for (int bigger = 0; bigger <= 9; bigger++)
-                    {
-                        increase[i, current] = (increase[i, current] + increase[i - 1, bigger]);
-                    }
-                }
-                for (int k = 0; k < 9; k++)
-                {
-                    result += decrease[i, k] + increase[i, k];
-                }
-
-                result -= increase[i, 0];
-                result -= 10;
-            }
-
-            result += 1;
-            return result;
-        }
-
-        private BigInteger Factorial(BigInteger number, BigInteger start)
-        {
-            BigInteger result = BigInteger.One;
-            for (BigInteger i = start; i <= number; i++)
-            {
-                result *= i;
-            }
             return result;
         }
     }
