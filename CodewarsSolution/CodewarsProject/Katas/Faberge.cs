c02b714cba98fdca52d436e090665820eafd2328
Marco <Marco@DESKTOP-MLGG85L>
2018-10-23 16:22:21 +0200

    Faberge Completed

--- CodewarsSolution/CodewarsProject/Katas/Faberge.cs
+++ CodewarsSolution/CodewarsProject/Katas/Faberge.cs
@@ -4,37 +4,23 @@ namespace CodewarsProject
 {
     public class Faberge
     {
-        public static BigInteger Height(int n, int m)
+        public static BigInteger Height(BigInteger n, BigInteger m)
         {
-            if (n == 0 || m == 0)
+
+            if (BigInteger.Compare(n, BigInteger.Zero) == 0 || BigInteger.Compare(m, BigInteger.Zero) == 0)
+                return BigInteger.Zero;            
+
+            BigInteger result = 1;
+            BigInteger total = 0;
+
+            BigInteger value = 1;
+            for (BigInteger k = 1; k <= n; k++)
             {
-                return BigInteger.Zero;
+                value = value * (m - k + 1) / k;
+                total += value;
             }
 
-            BigInteger myNumber = BigInteger.One;
-
-            //myNumber = (m * (m + 1)) / 2;
-
-            while (n >= 2)
-            {
-                double temp = (double)((double)m * (m + 1.0)) / (double)Factorial(n);
-                myNumber *= temp > 0 ? (BigInteger)temp : 1;
-                m--;
-                n--;
-            }
-
-            return myNumber-1;
-        }
-
-        private static BigInteger Factorial(int n)
-        {
-            BigInteger result = BigInteger.One;
-            while (n > 1)
-            {
-                result *= n;
-                n--;
-            }
-            return result;
+            return total;
         }
     }
 }
