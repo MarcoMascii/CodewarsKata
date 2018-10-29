58c240d333cf838ead5d807eff3ab2885a1ca44f
Marco <Marco@DESKTOP-MLGG85L>
2018-10-29 10:29:45 +0100

    Added magnet kata

---
+++ CodewarsSolution/CodewarsProject/Katas/Magnets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodewarsProject
+{
+    public class Magnets
+    {
+        public static double Doubles(int maxk, int maxn)
+        {
+            double result = 0;
+            for (long k = 1; k <= maxk; k++)
+            {
+                for (long n = 1; n <= maxn; n++)
+                {
+                    result += 1.0 / (k * Math.Pow((double)(n + 1), 2 * k));
+                }
+            }
+            return result;
+        }
+    }
+}
