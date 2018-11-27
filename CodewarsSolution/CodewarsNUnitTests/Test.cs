6cfc4e763b24d5cc456badfc06fa8559fe8bb422
Marco Mascii <marcomascii@MBP-di-Marco.lan>
2018-11-27 09:06:49 +0100

    adding NUnit Test

---
+++ CodewarsSolution/CodewarsNUnitTests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace CodewarsNUnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ CodewarsSolution/CodewarsNUnitTests/Tests/AlphabeticAnagramsTest.cs
@@ -0,0 +1,45 @@
+﻿using CodewarsProject;
+using NUnit.Framework;
+
+namespace CodewarsNUnitTests.Tests
+{
+    [TestFixture]
+    public class AlphabeticAnagramsTest
+    {
+        [Test]
+        public void Test1()
+        {
+            TestNumberToOrdinal("A", 1);
+        }
+        [Test]
+        public void Test2()
+        {
+            TestNumberToOrdinal("ABAB", 2);
+        }
+        [Test]
+        public void Test3()
+        {
+            TestNumberToOrdinal("AAAB", 1);
+        }
+        [Test]
+        public void Test4()
+        {
+            TestNumberToOrdinal("BAAA", 4);
+        }
+        [Test]
+        public void Test5()
+        {
+            TestNumberToOrdinal("QUESTION", 24572);
+        }
+        [Test]
+        public void Test6()
+        {
+            TestNumberToOrdinal("BOOKKEEPER", 10743);
+        }
+
+        public void TestNumberToOrdinal(string value, long expected)
+        {
+            Assert.AreEqual(expected, AlphabeticAnagrams.ListPosition(value), string.Format("Input {0}", value));
+        }
+    }
+}
